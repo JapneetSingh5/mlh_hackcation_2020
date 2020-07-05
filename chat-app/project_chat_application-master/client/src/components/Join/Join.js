f4b6d728505a969556f25c470d1490320f091a14
Utkarsh Sharma <46284927+utkzas@users.noreply.github.com>
2020-07-05 12:06:44 +0530

    chat app commit

--- chat-app/project_chat_application-master/client/src/components/Join/Join.js
+++ chat-app/project_chat_application-master/client/src/components/Join/Join.js
@@ -10,7 +10,8 @@ export default function SignIn() {
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
+<h1 className="heading">MultiVerse</h1>
+        <font color ="white"><h3>Sit back, try to relax your shoulders, clear you mind, and say <b>Hi</b></h3></font>
         <div>
           <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
@@ -18,7 +19,7 @@ export default function SignIn() {
           <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
+          <button className={'button mt-20'} type="submit">Get, Set, Go</button>
         </Link>
       </div>
     </div>
